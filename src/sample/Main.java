74cd095ceff4a4b25bee98e022d9a660b8b8fafd
clcriado0 <carlos.larriba@metadata.es>
2020-10-26 00:32:42 +0100

    Modificacion del Login

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,21 +4,37 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.awt.*;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 1000, 600));
-        primaryStage.setResizable(false);
+        primaryStage.setScene(new Scene(root, 1280, 720));
+        primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.show();
+        primaryStage.setResizable(false);
+
+        Scene scene = primaryStage.getScene();
+        Pane panel = (Pane) scene.lookup("#panelMenu");
+
+        panel.setOnMousePressed(pressEvent -> {
+            panel.setOnMouseDragged(dragEvent -> {
+                primaryStage.setX(dragEvent.getScreenX() - pressEvent.getSceneX());
+                primaryStage.setY(dragEvent.getScreenY() - pressEvent.getSceneY());
+            });
+        });
     }
 
 
     public static void main(String[] args) {
         launch(args);
     }
+
 }
